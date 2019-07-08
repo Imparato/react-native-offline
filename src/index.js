9ecae2e34fc9ed0050708fd71e400c4bb73a5f6a
Colin Darie <colin@darie.eu>
2019-07-08 22:40:59 +0200

    Remove redux-saga dependency.

--- src/index.js
+++ src/index.js
@@ -17,9 +17,6 @@ module.exports = {
   get offlineActionTypes() {
     return require('./redux/actionTypes').default;
   },
-  get networkSaga() {
-    return require('./redux/sagas').default;
-  },
   get checkInternetConnection() {
     return require('./utils/checkInternetConnection').default;
   },
